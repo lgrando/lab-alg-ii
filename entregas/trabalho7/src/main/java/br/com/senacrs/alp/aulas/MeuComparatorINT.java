cd3ba87f8000658df26c89a14907da4f77110b1d
LUCAS <LUCAS@192.168.2.3>
2013-04-15 23:28:36 -0300

    trabalho7

--- entregas/trabalho7/src/main/java/br/com/senacrs/alp/aulas/MeuComparatorINT.java
+++ entregas/trabalho7/src/main/java/br/com/senacrs/alp/aulas/MeuComparatorINT.java
@@ -7,7 +7,25 @@ public class MeuComparatorINT implements Comparator<Integer> {
 	@Override
 	public int compare(Integer o1, Integer o2) {
 		
-		return 0;
+		int resultado = 0;
+		if (o1 == null && o2 == null) {
+			resultado = 0;
+		}else if (o1%2 == 0){
+			if(o2 < o1){
+				resultado =  -1;
+			}else{
+				resultado = 1;
+			}
+		}else if(o2%2 != 0){
+			if(o2 < o1){
+				resultado = 1;
+			}else{
+				resultado = -1;
+			}
+		} else {
+			resultado = o2.compareTo(o1);
+		}
+		return resultado;
 	}
 
 	
--- entregas/trabalho7/src/main/java/br/com/senacrs/alp/aulas/MeuComparatorSTRING.java
+++ entregas/trabalho7/src/main/java/br/com/senacrs/alp/aulas/MeuComparatorSTRING.java
@@ -7,7 +7,15 @@ public class MeuComparatorSTRING implements Comparator<String> {
 	@Override
 	public int compare(String o1, String o2) {
 		
-		return 0;
+		if(o1 == null && o2 == null){
+			return 0;
+		}else if(o1 != o2){
+			return -1;
+		}else if(o1 == o2){
+			return 1;
+		}else{
+			return o2.compareTo(o1);
+		}
 	}
 
 }
