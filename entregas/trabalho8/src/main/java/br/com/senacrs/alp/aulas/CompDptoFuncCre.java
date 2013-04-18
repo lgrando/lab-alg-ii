58ba4549a93c7f7a382e3c94804de102e4f27296
631220019 <631220019@Sala603-09.fspoaeduc.com.br>
2013-04-18 20:04:32 -0300

    trabalho8

---
+++ entregas/trabalho8/src/main/java/br/com/senacrs/alp/aulas/CompDptoFuncCre.java
@@ -0,0 +1,13 @@
+package br.com.senacrs.alp.aulas;
+
+import java.util.Comparator;
+
+public class CompDptoFuncCre implements Comparator<Departamento> {
+
+	@Override
+	public int compare(Departamento arg0, Departamento arg1) {
+		
+		return 0;
+	}
+
+}
---
+++ entregas/trabalho8/src/main/java/br/com/senacrs/alp/aulas/CompFuncOrdAlf.java
@@ -0,0 +1,13 @@
+package br.com.senacrs.alp.aulas;
+
+import java.util.Comparator;
+
+public class CompFuncOrdAlf implements Comparator<Funcionario>{
+
+	@Override
+	public int compare(Funcionario o1, Funcionario o2) {
+		
+		return 0;
+	}
+
+}
