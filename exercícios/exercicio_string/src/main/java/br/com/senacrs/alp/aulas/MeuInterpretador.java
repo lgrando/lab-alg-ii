d38ffd16e5f24ed159d5fb2ce6316e88f70d722f
631220019 <631220019@Sala603-09.fspoaeduc.com.br>
2013-04-25 20:38:14 -0300

    exercicio_string

--- exercícios/exercicio_string/src/main/java/br/com/senacrs/alp/aulas/MeuInterpretador.java
+++ exercícios/exercicio_string/src/main/java/br/com/senacrs/alp/aulas/MeuInterpretador.java
@@ -1,5 +1,11 @@
 package br.com.senacrs.alp.aulas;
 
-public class MeuInterpretador  {
+public class MeuInterpretador implements InterpretadorStrings{
+
+	@Override
+	public TIPO_STRING lerString(String str) {
+		
+		return null;
+	}
 
 }
